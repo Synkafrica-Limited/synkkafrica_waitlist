784a89fa497ea69c7211683491976c01f6373a28
Musecreatives <paulsolaeniolawun@gmail.com>
2025-07-26 08:27:42 +0100

    sendgrd legacy template

--- netlify/functions/waitlist.js
+++ netlify/functions/waitlist.js
@@ -28,7 +28,7 @@ exports.handler = async function(event) {
         email: 'no-reply@synkkafrica.com',
         name: 'Synkkafrica Team'
       },
-      templateId: ' d-c16b0c5a05e04a2183bf84044764d622',
+      templateId: 'd-c16b0c5a05e04a2183bf84044764d622',
       dynamic_template_data: {
         name,
         email,
